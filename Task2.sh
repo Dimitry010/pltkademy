a27a1da0b7abdaa3964a8f1416a1cda669508a6f
Dimitry010 <90064362+Dimitry010@users.noreply.github.com>
2021-09-04 16:04:15 +0300

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -1,7 +1,14 @@
+
+# to schedule the script i'll add cron job via crontab -e
+and passing the line for example (0 0 * * 1) /pass to file /task1.sh
+
 #! /bin/bash
+ 
 for n in {1..10};
-do      
-        timestamp=$(date +"%T");
-        tr -dc A-Za-z0-9 </dev/urandom | head -c 1000 > test_$timestamp\_$n.txt;
+
+do 	
+	timestamp=$(date +"%T");
+	tr -dc A-Za-z0-9 </dev/urandom | head -c 1000 > test_$timestamp_$n;
 done
+
 
